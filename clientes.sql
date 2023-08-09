21dd611f009e02f09f93e2729e9276ec724969e8
greta <greta.pereda@gmail.com>
2023-08-09 12:28:28 -0600

    tp

---
+++ clientes.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-08-2023 a las 20:25:59
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `java`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `email` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `telefono`, `email`) VALUES
+(2, 'Mia', 'Marti', '351789456', 'mia.m@mail.com'),
+(12, 'Carolina', 'Marti', '351741852', 'caromarti@mail '),
+(13, 'Greta', 'Pereda', '351963852', 'greta.p@gmail.com'),
+(15, 'Santiago', 'Dehez', '3543789456', 'santid@mail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/main/java/com/mycompany/sistemag/dao/ClienteDao.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemag.dao;
+
+import com.mycompany.sistemag.models.Cliente;
+import com.mysql.jdbc.StringUtils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author GT
+ */
+public class ClienteDao {
+    
+    public Connection conectar(){
+        String baseDeDatos = "java";
+        String usuario = "root";
+        String password = "";
+        String host = "localhost";
+        String puerto = "3306";
+        String driver = "com.mysql.jdbc.Driver";
+        String conexionUrl = "jdbc:mysql://" + host + ":" + puerto + "/" + baseDeDatos + "?useSSL=false";
+        
+        Connection conexion = null;
+        
+        try {
+            Class.forName(driver);
+            conexion = DriverManager.getConnection(conexionUrl, usuario, password);
+   
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conexion;
+    }
+    
+    public void agregar(Cliente cliente) throws SQLException{
+
+        try {
+            Connection conexion = conectar();
+            String sql = "INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `telefono`, `email`) VALUES (NULL, '" + cliente.getNombre() + "', '" + cliente.getApellido() + "', '" + cliente.getTelefono() + "', '" + cliente.getEmail() + "');";
+            Statement statement = conexion.createStatement();
+            statement.execute(sql); 
+        
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+    }
+    
+    public void actualizar(Cliente cliente) throws SQLException{
+
+        try {
+            Connection conexion = conectar();
+            String sql = "UPDATE `clientes` SET `nombre` = '" + cliente.getNombre() +"', `apellido` = '" + cliente.getApellido() +"', `telefono` = '" + cliente.getTelefono() +"', `email` = '" + cliente.getEmail() +"' WHERE `clientes`.`id` = " + cliente.getId() +";";
+            Statement statement = conexion.createStatement();
+            statement.execute(sql); 
+        
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }  
+    }
+    
+     public List<Cliente> listar(){
+        List<Cliente> listado = new ArrayList<>();
+        
+        try {
+            Connection conexion = conectar();
+        
+            String sql = "SELECT * FROM `clientes`";
+            Statement statement = conexion.createStatement();
+            ResultSet resultado = statement.executeQuery(sql); 
+            
+            while(resultado.next()){
+                Cliente cliente = new Cliente();
+                cliente.setId(resultado.getString("id"));
+                cliente.setNombre(resultado.getString("nombre"));
+                cliente.setApellido(resultado.getString("apellido"));
+                cliente.setTelefono(resultado.getString("telefono"));
+                cliente.setEmail(resultado.getString("email"));
+                listado.add(cliente);
+            }
+        
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return listado;
+        
+    }
+     
+     public void eliminar(String id) throws SQLException{
+      
+        try {
+            Connection conexion = conectar();
+            String sql = "DELETE FROM `clientes` WHERE `clientes`.`id` = " + id;
+            
+            Statement statement = conexion.createStatement();
+            statement.execute(sql); 
+        
+        } catch (Exception ex) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+
+    public void guardar(Cliente cliente) throws SQLException {
+        if(StringUtils.isEmptyOrWhitespaceOnly(cliente.getId())){
+            agregar(cliente);
+        }else {
+            actualizar(cliente);
+        }
+    }
+} 
